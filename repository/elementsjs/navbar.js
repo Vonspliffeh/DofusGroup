ec5907fe5174ceb3a738f02a79be8e488a4c2a20
Alexandre <alexandre.dejour1@gmail.com>
2024-11-23 17:17:51 +0100

    chemin entre les pages

--- repository/elementsjs/navbar.js
+++ repository/elementsjs/navbar.js
@@ -3,10 +3,10 @@ export function createNavbar() {
   nav.innerHTML = `
     <ul class="navbar">
       <li><a href="index.html">Acceuil</a></li>
-      <li><a href="groupe.html">Groupe</a></li>
+      <li><a href="pageshtml/groupe.html">Groupe</a></li>
       <li><img class="logoNavbar" src="images/logoDofus.webp" alt="logo Dofus"></li>
-      <li><a href="communauté.html">Communauté</a></li>
-      <li><a href="compte.html">Compte</a></li>
+      <li><a href="pageshtml/communauté.html">Communauté</a></li>
+      <li><a href="pageshtml/compte.html">Compte</a></li>
     </ul>
   `;
   document.querySelector('header').prepend(nav);
