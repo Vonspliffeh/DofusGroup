31d89b4b8407e5a4dd14cc6154d34c9c4974fbcd
Alexandre <alexandre.dejour1@gmail.com>
2024-11-25 13:28:14 +0100

    sauvegarde avant modification construction footer

--- repository/jsElements/footer.js
+++ repository/jsElements/footer.js
@@ -1,7 +1,7 @@
 export function createFooter() {
   const footer = document.createElement('footer');
   footer.innerHTML = `
-    <ul class="navbar">
+    <ul class="footer">
       <li><a href="index.html">DofusGroup</a></li>
       <li><a href="communauté.html">Communauté</a></li>
       <li><a href="communauté.html">Autres liens</a></li>
